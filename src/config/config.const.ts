c63e80502af3217cb2070fc1d87dc60f1c09fa06
Marco <marcohavermans+github@hotmail.nl>
2017-12-20 11:05:50 +0100

    Fix lint errors

--- src/config/config.const.ts
+++ src/config/config.const.ts
@@ -3,7 +3,7 @@ import { IConfig } from './config.interface';
 const config: IConfig = require('../../config/config');
 
 export const Config: IConfig = {
-    port: (process.env.PORT || config.port) as number,
+    allowOrigin: process.env.ALLOW_ORIGIN || config.allowOrigin,
     mongoDbUri: process.env.MONGODB_URI || config.mongoDbUri,
-    allowOrigin: process.env.ALLOW_ORIGIN || config.allowOrigin
+    port: (process.env.PORT || config.port) as number
 };
