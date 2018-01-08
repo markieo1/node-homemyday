e9527a206177eefbbd1703542fe9e5206fe0aef5
Kevin Ly <kevinly92@hotmail.com>
2018-01-08 14:20:45 +0100

    Add delete method

--- src/api/v1/accommodation.routes.ts
+++ src/api/v1/accommodation.routes.ts
@@ -1,7 +1,8 @@
 import express = require('express');
-
+import * as mongoose from 'mongoose';
 import { Accommodation } from '../../model/accommodation.model';
 import { expressAsync } from '../../utils/express.async';
+import { ApiError } from '../errors/api.error';
 
 const routes = express.Router();
 
@@ -15,6 +16,19 @@ routes.get('/:id', expressAsync(async (req, res, next) => {
     const accommodation = await Accommodation.findById(req.params.id);
 
     res.send(accommodation);
+}));
+
+routes.delete('/:id', expressAsync(async (req, res, next) => {
+    const accommodationId = req.params.id;
+
+    const isValidId = mongoose.Types.ObjectId.isValid(accommodationId);
+    if (!isValidId) {
+        throw new ApiError(400, 'Invalid id supplied!');
+    }
+
+    await Accommodation.remove({ _id: accommodationId });
+
+    res.send(204).send();
 }));
 
 export default routes;
