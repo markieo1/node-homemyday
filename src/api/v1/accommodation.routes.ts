8b08d4d91f6628183abdcd545c96361140e878d5
Danny Tam <kenwai2010@gmail.com>
2018-01-08 14:33:16 +0100

    added post accomodation (wip)

--- src/api/v1/accommodation.routes.ts
+++ src/api/v1/accommodation.routes.ts
@@ -1,6 +1,7 @@
 import express = require('express');
 
 import { Accommodation } from '../../model/accommodation.model';
+import { IAccommodationDocument } from '../../model/schemas/accommodation.schema';
 import { expressAsync } from '../../utils/express.async';
 
 const routes = express.Router();
@@ -15,6 +16,17 @@ routes.get('/:id', expressAsync(async (req, res, next) => {
     const accommodation = await Accommodation.findById(req.params.id);
 
     res.send(accommodation);
+}));
+
+routes.post('/', expressAsync(async (req, res, next) => {
+    const reqBody = req.body;
+    
+    const newAccomodation = {
+        
+    } as IAccommodationDocument;
+
+    const accommodation = await Accommodation.create();
+    res.send();
 }));
 
 export default routes;
