28ad7f46a594d6a1e8fb44c662451a7700a0ee0f
Danny Tam <kenwai2010@gmail.com>
2018-01-08 14:52:24 +0100

    added newAccomodation prop

--- src/api/v1/accommodation.routes.ts
+++ src/api/v1/accommodation.routes.ts
@@ -22,11 +22,28 @@ routes.post('/', expressAsync(async (req, res, next) => {
     const reqBody = req.body;
 
     const newAccomodation = {
-
+        name: reqBody.name,
+        description: reqBody.description,
+        maxPersons: reqBody.maxPersons,
+        continent: reqBody.continent,
+        country: reqBody.country,
+        location: reqBody.location,
+        latitude: reqBody.latitude,
+        longitude: reqBody.longitude,
+        rooms: reqBody.rooms,
+        beds: reqBody.beds,
+        recommended: reqBody.recommended,
+        price: reqBody.price,
+        spaceText: reqBody.spaceText,
+        servicesText: reqBody.servicesText,
+        pricesText: reqBody.pricesText,
+        rulesText: reqBody.rulesText,
+        cancellationText: reqBody.cancellationText
     } as IAccommodationDocument;
 
-    const accommodation = await Accommodation.create();
-    res.send();
+    const accommodation = await Accommodation.create(newAccomodation);
+
+    res.status(201).send(accommodation);
 }));
 
 export default routes;
