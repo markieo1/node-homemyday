74e5f49493577b5a4502c127cf589528a41621a9
Hugo Zink <hugo.zink@hotmail.com>
2018-01-09 12:05:16 +0100

    Added registration functionality.

--- src/api/v1/authentication.routes.ts
+++ src/api/v1/authentication.routes.ts
@@ -1,27 +1,46 @@
+import * as bcrypt from 'bcrypt';
 import express = require('express');
 import sanitize = require('mongo-sanitize');
-import * as bcrypt from 'bcrypt';
 import User from '../../model/user.model';
+import { expressAsync } from '../../utils/express.async';
 
-const router = express.Router();
+const routes = express.Router();
 
 //
 // Retourneer een specifieke user. Hier maken we gebruik van URL parameters.
 // Vorm van de URL: http://hostname:3000/api/v1/authentication/23
 //
-router.get('/authentication', function (req, res) {
+routes.get('/authentication', (req, res) => {
     res.contentType('application/json');
     const usr = sanitize(req.body.username);
     const pwd = sanitize(req.body.password);
 
-    
 
     User.findOne({username: usr}).then((user) => {
 
-        //res.status(200).json(user);
+        // res.status(200).json(user);
     }).catch((error) => {
         res.status(400).json(error);
     });
 });
 
-export default router;
+routes.post('/register', expressAsync(async (req, res, next) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Hash the password for 10 rounds
+    const hash = await bcrypt.hash(password, 10);
+
+    const user = new User({
+        username,
+        password: hash,
+        role: 'User'
+    });
+
+    await user.save();
+
+    res.status(204).end();
+}));
+
+export default routes;
--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,5 +1,5 @@
 import { Document, Model, model, Schema } from 'mongoose';
-import { UserSchema, IUserDocument } from './schemas/user.schema';
+import { IUserDocument, UserSchema } from './schemas/user.schema';
 
 export interface IUserModel extends Model<IUserDocument> { }
 export const User = model<IUserDocument, IUserModel>('User', UserSchema);
