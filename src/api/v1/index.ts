a22d27df30b3331fb2889c18b7a07dd2b60e4f69
Hugo Zink <hugo.zink@hotmail.com>
2018-01-09 12:44:20 +0100

    Fixed linting error.

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,10 +1,10 @@
 import express = require('express');
-import routesAuthentication from './authentication.routes';
 import accommodationRoutes from './accommodation.routes';
+import authenticationRoutes from './authentication.routes';
 
 const router = express.Router();
 
-router.use('/authentication', routesAuthentication);
+router.use('/authentication', authenticationRoutes);
 router.use('/accommodations', accommodationRoutes);
 
 export default router;
