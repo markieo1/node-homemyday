fabb40d7dd1a78ba0f2d89304754830c80be1fb3
Hugo Zink <hugo.zink@hotmail.com>
2018-01-09 10:21:45 +0100

    Fixed invalid JSDoc.

--- src/service/accommodation.service.ts
+++ src/service/accommodation.service.ts
@@ -29,7 +29,7 @@ export class AccommodationService {
         return await Accommodation.findByIdAndUpdate(id, accommodation, { new: true });
     }
 
-    /*
+    /**
      * Deletes an accommodation of the ID
      * @param id The Object ID to delete.
      */
