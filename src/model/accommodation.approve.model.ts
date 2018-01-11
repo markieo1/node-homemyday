94c685547dfbd3588938bed0acd09d85b3550bf5
Danny Tam <kenwai2010@gmail.com>
2018-01-11 10:50:41 +0100

    fix approval moel

--- src/model/accommodation.approve.model.ts
+++ src/model/accommodation.approve.model.ts
@@ -1,8 +1,8 @@
 import { Document, Model, model, Schema } from 'mongoose';
 import { AccommodationApproveSchema, IAccommodationApproveDocument } from './schemas/accommodation.approve.schema';
 
-export interface IAccommodationModel extends Model<IAccommodationApproveDocument> { }
+export interface IAccommodationApproveModel extends Model<IAccommodationApproveDocument> { }
 export const AccommodationApprove = model<IAccommodationApproveDocument,
-                IAccommodationModel>('AccommodationApprove', AccommodationApproveSchema);
+        IAccommodationApproveModel>('AccommodationApprove', AccommodationApproveSchema);
 
 export default AccommodationApprove;
