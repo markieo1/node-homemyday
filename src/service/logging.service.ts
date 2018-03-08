a4909e197ae43531977dd3499e2583ef75f5e058
Unknown <kly1@avans.nl>
2018-03-08 16:29:58 +0100

    add path for logger file

--- src/service/logging.service.ts
+++ src/service/logging.service.ts
@@ -8,7 +8,7 @@ export class LoggingService {
             level: 'info',
             format: winston.format.json(),
             transports: [
-                new winston.transports.File({ filename: 'info.log'})
+                new winston.transports.File({ filename: './info.log'})
             ]
         });
     }
